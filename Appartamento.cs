c86c78e2a158a04421cfcebb40ed854e9eb16ef4
Fraulo23 <fraulinifederico@gmail.com>
2023-11-30 17:27:49 +0100

    Aggiungere i file di progetto.

---
+++ Appartamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_28._3
+{
+    internal class Appartamento:Immobile
+    {
+        public int nVani { get; set; }
+        public int nBagni { get; set; }
+    }
+}
---
+++ Immobile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_28._3
+{
+    internal class Immobile
+    {
+        public string CodiceAlfanumerico { get; set; }
+        public string indirizzo { get; set; }
+        public string cap { get; set; }
+        public string citta { get; set; }
+        public int superficie { get; set; }
+    }
+}
